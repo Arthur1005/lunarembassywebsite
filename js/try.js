ec58795252cf6e3848f48da467ca6c2928073f62
Arthur1005 <linsy1005@gmail.com>
2025-06-05 22:27:26 +1000

    clear up files

--- js/try.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("searchBox").addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    const query = this.value.trim();
-    if (query) {
-      window.location.href = `searchResult.html?q=${encodeURIComponent(query)}`;
-    }
-  }
-});
